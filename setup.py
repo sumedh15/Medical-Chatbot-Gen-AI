34278cbbcb88e4d9968b93e35961ef5e04b426ef
sumedh <sumedh15@gmail.com.com>
2025-08-11 23:08:38 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'sumedh',
+    author_email= 'sumedh@abc.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
